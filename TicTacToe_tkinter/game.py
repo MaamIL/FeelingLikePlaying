ae6ef680027020c0a3d7973402a1685c2ebce27c
Mosheat Alon-Moses <contact.mosheat@gmail.com>
2024-12-25 15:07:37 +0200

    Add files via upload

---
+++ TicTacToe_tkinter/game.py
@@ -0,0 +1,9 @@
+from TicTacToe_tkinter.TicTacToe import TicTacToe
+
+def main():
+    #instace the TicTacToe class and run
+    game_instance = TicTacToe()
+    game_instance.mainloop()
+
+if __name__ == "__main__":
+    main()
